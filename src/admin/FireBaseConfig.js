6584155999164056f11d5feb8081776e37948a40
Nicolas Cerecedo <nm.cerecedo@outlook.com>
2025-04-28 13:46:36 -0300

    css

--- src/admin/FireBaseConfig.js
+++ src/admin/FireBaseConfig.js
@@ -1,6 +1,3 @@
-"use client";
-
-
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
@@ -21,20 +18,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);  // Autenticación
 export const storage = getStorage(app); // Imagenes
 export const baseDeDatos = getFirestore(app); // Base de datos Firestore
-
-// // Verifica si el entorno es compatible con analytics y si estamos en el cliente
-// let analytics = null;
-// if (typeof window !== 'undefined') {
-//   const isAnalyticsSupported = async () => {
-//     if (await isSupported()) {
-//       return getAnalytics(app);
-//     }
-//     return null;
-//   };
-  
-//   isAnalyticsSupported().then((result) => {
-//     analytics = result;
-//   });
-// }
-
-// export { analytics };
